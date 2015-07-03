07abe149f64377401cf36e90642136af9f96ee64
zhanggeng <zhanggengdyx@163.com>
2015-07-03 11:07:22 +0800

    1、修改翻译的内容，里面的话很不通顺；2、添加SettingActivity完成设置界面

--- Application_Settings/app/src/main/java/com/example/zhanggeng/application_settings/MainActivity.java
+++ Application_Settings/app/src/main/java/com/example/zhanggeng/application_settings/MainActivity.java
@@ -1,22 +1,43 @@
 package com.example.zhanggeng.application_settings;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button btn_for_settings;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_for_settings = (Button) findViewById(R.id.btn_for_settings);
+        btn_for_settings.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(context, SettingActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+
+
         // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingFragment())
-                .commit();
+//        getFragmentManager().beginTransaction()
+//                .replace(android.R.id.content, new SettingFragment())
+//                .commit();
     }
 
     @Override
---
+++ Application_Settings/app/src/main/java/com/example/zhanggeng/application_settings/SettingActivity.java
@@ -0,0 +1,27 @@
+package com.example.zhanggeng.application_settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * 
+ * TODO: 设置界面
+ * 
+ * @author : zhanggeng
+ * @date : 2015年4月9日
+ * @version : v1.0 -----------修订历史-----------
+ */
+public class SettingActivity extends PreferenceActivity  {
+
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+
+	}
+
+
+
+}
